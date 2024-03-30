97cb3c52b57e3ad7ef4befc36c4a92449bfb7b9d
jazel123 <pangasianjazel@gmail.com>
2024-03-30 10:35:49 +0800

    first commit

---
+++ portfolio/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/Footer.vue'
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+    <header>
+        <Header />
+    </header>
+      <div class="wrapper">
+        <HelloWorld msg="ANNA MAE MONTECALVO" />
+        <img src="@/components/icons/anna.jpg" alt="Photo of Anna Mae" class="profile">
+      </div>
+
+
+    <footer>
+      <Footer />
+    </footer>
+
+  </template>
+
+
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.profile {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+</style>
---
+++ portfolio/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h6>Anna Mae Montecalvo</h6>
+    </div>
+  </template>
+  <style scoped>
+  h6{
+    color: black;
+  }
+  </style>
+
+
